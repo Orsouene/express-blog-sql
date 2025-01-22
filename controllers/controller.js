ef5b5d8a39c4ed9dc0a0c6668434ce24abedb46b
Orsouene <orsoueneelaouizeb@gmail.com>
2025-01-22 17:04:46 +0100

    Milestone 2 (Verificato su Postman )

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,18 @@
 const connection = require("../connection");
 
 // INDEX
-function index(req, res) {}
+function index(req, res) {
+  // Dichiaro la mia query
+  const sql = "SELECT * FROM db_blog.posts";
+  // eseguo la query SQL
+  connection.query(sql, (err, results) => {
+    // se c'è un errore nell esecuzione ella query
+    if (err) return res.status(500).json({ error: "DB QUERY FAILED" });
+    // se la query è andata a buon fine
+    console.log(results);
+    res.json(results);
+  });
+}
 
 // SHOW
 function show(req, res) {}
